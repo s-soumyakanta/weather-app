5a7d99b22f1c817ae0f34ced6bd3e0f5b7faaedc
S Soumyakanta <mr.ssoumyakanta@gmail.com>
2023-05-07 17:47:02 +0530

    footer updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
     <>
  <div className='flex felx-col justify-between px-2  space-x-10 md:text-base text-sm  mt-2 
                     shadow-sm  rounded-md'>
-  <div>Developed by <span className='dark:text-blue-500 text-blue-800'><a href="https://s-soumyakanta.com" target='_blank' > S Soumyakanta </a></span></div>
-  <div><a href="https://github.com/s-soumyakanta/weather-app"  target='_blank'> Source Code</a></div>
+  <div>Developed by <span className='dark:text-blue-500 text-blue-800'><a href="https://s-soumyakanta.com"> S Soumyakanta </a></span></div>
+  <div><a href="https://github.com/s-soumyakanta/weather-app" > Source Code</a></div>
  </div>
     </>
   )
